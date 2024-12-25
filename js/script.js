56092d25e0977bc2aea428ee2d92d113df9d1c6a
OSCAR REYES <oscarreyesponciano@gmail.com>
2024-12-24 18:11:03 -0600

    Version 1

---
+++ js/script.js
@@ -0,0 +1,25 @@
+const body = document.querySelector('body')
+
+const crearNieve = () => {
+    let copo = document.createElement('i')
+    let x = innerWidth * Math.random()
+    let size = (Math.random() * 8) + 2
+    let z = Math.round(Math.random()) * 100
+    let delay = Math.random() * 5
+    let animation = (Math.random() * 10) + 5
+
+    copo.style.left = x + 'px'
+    copo.style.width = size + 'px'
+    copo.style.height = size + 'px'
+    copo.style.zIndex = z
+    copo.style.animationDelay = delay + 's'
+    copo.style.animationDuration = animation + 's'
+
+    body.appendChild(copo)
+
+    setTimeout(() => {
+        copo.remove()
+    }, animation * 1000)
+}
+
+setInterval(crearNieve, 70)
